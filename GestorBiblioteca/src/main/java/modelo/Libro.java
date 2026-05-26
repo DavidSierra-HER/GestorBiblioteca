80ba7e5e0d4aac1ec9f1ea753b7b56a0404ec8e5
David <david.sieher@educa.jcyl.es>
2026-05-26 19:26:24 +0200

    modelo preparado, con javadoc, y comentarios

--- GestorBiblioteca/src/main/java/modelo/Libro.java
+++ GestorBiblioteca/src/main/java/modelo/Libro.java
@@ -1,5 +1,156 @@
 package modelo;
 
-public class Libro {
+import java.util.Objects;
 
+/**
+ * Está es la clase principal del objeto libro donde se reciben sus parametros
+ * 
+ * @author  David
+ * @version 1.0
+ * @since   2026-05-26
+ */
+
+public class Libro {
+	
+
+	
+//	atributos de la clase libro.
+	
+	private String isbn;
+	private String titulo;
+	private String autor;
+	private String genero;
+	private String anno;
+	private int disponibles;
+	
+	
+	
+	//constructor
+	public Libro(String isbn, String titulo, String autor, String genero, String anno, int disponibles) {
+		super();
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.genero = genero;
+		this.anno = anno;
+		this.disponibles = disponibles;
+	}
+
+
+
+	//getters y setters
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+
+
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+
+
+	public String getAutor() {
+		return autor;
+	}
+
+
+
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+
+
+
+	public String getGenero() {
+		return genero;
+	}
+
+
+
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+
+
+
+	public String getAnno() {
+		return anno;
+	}
+
+
+
+
+	public void setAnno(String anno) {
+		this.anno = anno;
+	}
+
+
+
+
+	public int getDisponibles() {
+		return disponibles;
+	}
+
+
+
+
+	public void setDisponibles(int disponibles) {
+		this.disponibles = disponibles;
+	}
+
+
+
+	//orientamos el hash y el equals sobre el ibn que es donde vamos a poder identificar más facilmente
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(isbn);
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		return Objects.equals(isbn, other.isbn);
+	}
+
+
+
+	//ToString 
+	@Override
+	public String toString() {
+		return "Libro [isbn=" + isbn + ", titulo=" + titulo + ", autor=" + autor + ", genero=" + genero + ", año="
+				+ anno + ", disponibles=" + disponibles + "]";
+	}
 }
--- GestorBiblioteca/src/main/java/modelo/Prestamo.java
+++ GestorBiblioteca/src/main/java/modelo/Prestamo.java
@@ -1,5 +1,169 @@
 package modelo;
 
-public class Prestamo {
+import java.time.LocalDate;
+import java.util.Objects;
 
+/**
+ * Está es la clase principal del objeto prestamo donde se reciben sus parametros
+ * 
+ * @author  David
+ * @version 1.0
+ * @since   2026-05-26
+ */
+
+public class Prestamo {
+	
+	//atributos de la clase prestamo
+	private String id;
+	private String estado;
+	private LocalDate fechaPrestado;
+	private LocalDate devolucionEstimada;
+	private LocalDate fechaDevolucion;
+	private Libro libroPrestado;
+	private Socio socioPrestamo;
+	
+	
+	//constructor
+	public Prestamo(String id, String estado, LocalDate fechaPrestado, LocalDate devolucionEstimada,
+			LocalDate fechaDevolucion, Libro libroPrestado, Socio socioPrestamo) {
+		super();
+		this.id = id;
+		this.estado = estado;
+		this.fechaPrestado = fechaPrestado;
+		this.devolucionEstimada = devolucionEstimada;
+		this.fechaDevolucion = fechaDevolucion;
+		this.libroPrestado = libroPrestado;
+		this.socioPrestamo = socioPrestamo;
+	}
+	
+	
+
+
+	public String getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+
+
+	public LocalDate getFechaPrestado() {
+		return fechaPrestado;
+	}
+
+
+
+
+	public void setFechaPrestado(LocalDate fechaPrestado) {
+		this.fechaPrestado = fechaPrestado;
+	}
+
+
+
+
+	public LocalDate getDevolucionEstimada() {
+		return devolucionEstimada;
+	}
+
+
+
+
+	public void setDevolucionEstimada(LocalDate devolucionEstimada) {
+		this.devolucionEstimada = devolucionEstimada;
+	}
+
+
+
+
+	public LocalDate getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+
+
+
+
+	public void setFechaDevolucion(LocalDate fechaDevolucion) {
+		this.fechaDevolucion = fechaDevolucion;
+	}
+
+
+
+
+	public Libro getLibroPrestado() {
+		return libroPrestado;
+	}
+
+
+
+
+	public void setLibroPrestado(Libro libroPrestado) {
+		this.libroPrestado = libroPrestado;
+	}
+
+
+
+
+	public Socio getSocioPrestamo() {
+		return socioPrestamo;
+	}
+
+
+
+
+	public void setSocioPrestamo(Socio socioPrestamo) {
+		this.socioPrestamo = socioPrestamo;
+	}
+
+
+
+
+	//hascode y equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prestamo other = (Prestamo) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+
+	//ToString donde trabajamos con los resultados del prestamo
+	@Override
+	public String toString() {
+		return "Prestamo [id=" + id + ", prestado=" + estado + ", fechaPrestado=" + fechaPrestado
+				+ ", devolucionEstimada=" + devolucionEstimada + ", fechaDevolucion=" + fechaDevolucion
+				+ ", libroPrestado=" + libroPrestado + ", socioPrestamo=" + socioPrestamo + "]";
+	}
+	
 }
