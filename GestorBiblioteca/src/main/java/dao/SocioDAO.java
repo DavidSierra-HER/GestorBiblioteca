010f49b55bd3e20a9e454c99d3190e146b347b0c
David <david.sieher@educa.jcyl.es>
2026-05-27 23:50:03 +0200

    SocioDAO completado tanto la interfaz como la implementación con sus respectivos metodos

--- GestorBiblioteca/src/main/java/dao/SocioDAO.java
+++ GestorBiblioteca/src/main/java/dao/SocioDAO.java
@@ -1,13 +1,25 @@
 package dao;
 
+import java.util.List;
+import modelo.Socio;
+
+/**
+ * Está es la clase principal de interfaz de los Socios.
+ * 
+ * @author  David
+ * @version 1.0
+ * @since   2026-05-26
+ */
+
+
 public interface SocioDAO {
 	
 	
-	public void obtenerSocio();
-	public void obtenerSocios();
-	public void modificarSocio();
-	public void eliminarSocio();
-	public void buscarDNI();
-	public void obtenerTlfn();
+	public List<Socio> obtenerSocio();
+	public boolean registrarSocio(Socio socio);
+	public boolean modificarSocio(Socio socio);
+	public void eliminarSocio(String dni);
+	public Socio buscarDNI(String dni);
+	
 
 }
