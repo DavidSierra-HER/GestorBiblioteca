fd360512f0262e507220bd53f21e3f0d4653f577
David <david.sieher@educa.jcyl.es>
2026-05-31 18:50:23 +0200

    menu para mostrar los tres paneles que hemos creado aparte, el usuario podrá moverse entre Libro, socio y prestamo

--- GestorBiblioteca/src/main/java/vista/VentanaPrincipal.java
+++ GestorBiblioteca/src/main/java/vista/VentanaPrincipal.java
@@ -3,6 +3,7 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 public class VentanaPrincipal {
 
@@ -36,8 +37,18 @@ public class VentanaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1024, 728);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		//menu para mostrar los tres paneles que hemos creado aparte
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 1024, 768);
+		frame.getContentPane().add(tabbedPane);
+		
+		//añadimos las pestañas 
+		tabbedPane.addTab("Libros", null, new PanelLibro(), null);
+		tabbedPane.addTab("Socios", null, new PanelSocio(), null);
+		tabbedPane.addTab("Prestamos", null, new PanelPrestamo(), null);
 	}
-
 }
